868a69354c8f94f560f2a90a43082da5da2c8c72
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-03-26 16:15:23 +0300

    VM setup scripts

--- yc_compute_instance_create.sh
+++ yc_compute_instance_create.sh
@@ -6,7 +6,7 @@ yc compute instance create \
   --cores=2 \
   --core-fraction=20 \
   --zone=ru-central1-a \
-  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-2004-lts,size=15GB \
+  --create-boot-disk image-folder-id=standard-images,image-family=ubuntu-1604-lts,size=10GB \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4 \
   --metadata serial-port-enable=1 \
   --ssh-key ~/.ssh/appuser.pub
