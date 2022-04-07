50b324ba9d18f711c756e42cf18636bfc11dec76
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-04-07 12:50:57 +0300

    Shell script for reddit-full instance creation

--- packer/scripts/create-reddit-vm.sh
+++ packer/scripts/create-reddit-vm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 yc compute instance create \
-  --name reddit-app \
-  --hostname reddit-app \
+  --name reddit-full \
+  --hostname reddit-full \
   --memory=4 \
   --cores=2 \
   --core-fraction=20 \
   --zone=ru-central1-a \
-  --create-boot-disk image-name=reddit-base-1649162103 \
+  --create-boot-disk image-name=reddit-full-1649323996 \
   --network-interface subnet-name=default-ru-central1-a,nat-ip-version=ipv4,nat-address=51.250.80.31 \
   --metadata serial-port-enable=1 \
-  --metadata-from-file user-data=packer/metadata.yaml
+  --ssh-key ~/.ssh/appuser.pub
