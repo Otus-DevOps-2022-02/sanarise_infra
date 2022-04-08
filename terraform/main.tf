1f2306f937ec496e6c508d14219cf9cdf355b36f
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-04-08 15:41:18 +0300

    Add resource for base reddit-app compute instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
+      # source = "yandex-cloud/yandex"
       vversion = "~>0.35"
     }
   }
@@ -9,8 +9,27 @@ terraform {
 }
 
 provider "yandex" {
-  token     = "AQAAAAAB0Zp8AATuwainFJr0tkI_lel5OmQqcQI"
-  cloud_id  = "b1gkqjl0oe48651f5mjj"
+  service_account_key_file = "secrets/key.json"
+  cloud_id = "b1gkqjl0oe48651f5mjj"
   folder_id = "b1gqp7711qhgjtc9l0mi"
-  zone      = "ru-central1-a"
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "reddit-app" {
+  name = "reddit-app"
+  resources {
+    cores = 2
+    memory = 2
+    core_fraction = 20
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd83mfr8osk59okkaslo"
+    }
+  }
+  network_interface {
+    subnet_id = "e9bkt8p86vv20gmjvos4"
+    nat = true
+    nat_ip_address = "51.250.82.15"
+  }
 }
