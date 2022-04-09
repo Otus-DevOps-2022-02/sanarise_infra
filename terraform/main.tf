aca2fce22e62a7164dfadd4fee08886b3a3379b1
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-04-09 19:39:52 +0300

    Simplify yandex provider declaration for older versions of terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,6 @@
 terraform {
   required_providers {
-    yandex = {
-      # source = "yandex-cloud/yandex"
-      version = "~>0.35"
-    }
+    yandex = "~>0.35"
   }
   required_version = "~> 0.12"
 }
