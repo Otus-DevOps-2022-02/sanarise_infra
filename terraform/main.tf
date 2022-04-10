8ec35ca0598b2950ea4dda24a0280173a6c3b443
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-04-10 18:17:28 +0300

    Fix yandex provider version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,17 @@
 terraform {
   required_providers {
-    yandex = "~>0.35"
+    yandex = "~> 0.35"
   }
   required_version = "~> 0.12"
 }
 
 provider "yandex" {
+  version = "~> 0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
 }
-
-# data "yandex_compute_image"
 
 resource "yandex_compute_instance" "reddit-app" {
   count    = var.reddit-app-count
