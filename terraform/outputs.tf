e5a35ee73e5ff7d0cb666c531fbc46bf24ff0524
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-04-13 15:10:18 +0300

    Add output for app-subnet.id

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
+output "app_subnet_id" {
+  value = yandex_vpc_subnet.app-subnet.id
+}
 output "external_ip_address_app" {
   value = yandex_compute_instance.app.*.network_interface.0.nat_ip_address
 }
