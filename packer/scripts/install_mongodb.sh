1a14cfbe615c849e5299e06f1783662715786a63
Aleksandr Chupakhin <sanarise@yandex.ru>
2022-04-13 14:52:43 +0300

    Make separate packer images for app and db

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+while pgrep -a apt-get; do echo 'Waiting for apt-get...'; sleep 1; done
 sudo apt-get --assume-yes install apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+while pgrep -a apt-get; do echo 'Waiting for apt-get...'; sleep 1; done
 sudo apt-get update
+while pgrep -a apt-get; do echo 'Waiting for apt-get...'; sleep 1; done
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
